2dc981a7678a15358ce2a0820b99a2b6ebf72397
Weng,David D <d.weng@student.fontys.nl>
2022-05-05 16:30:53 +0200

    lol

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {InlineProgramArgs} from "@pulumi/pulumi/automation";
+import {InlineProgramArgs, LocalWorkspace} from "@pulumi/pulumi/automation";
 import {PluginManager} from 'live-plugin-manager';
 
 // pkg = pulumi npm name https://www.npmjs.com/package/pulumi-cr-platform
 // version: latest version of npm module
 
-export async function getPackage(pkg:string, version:string) {
+async function getPackage(pkg:string, version:string) {
     const manager = new PluginManager();
 
     await manager.install(pkg, version);
@@ -12,8 +12,8 @@ export async function getPackage(pkg:string, version:string) {
     return app;
 }
 async function main() {
-    // it errors here i think
-    const kek = await getPackage('pulumi-cr-platform', '7.0.1');
+    // it errors here in function line 7 till line 12
+     const kek = await getPackage('pulumi-cr-platform', '7.0.1');
 
     const args: InlineProgramArgs = {
         stackName: 'henk',
@@ -21,9 +21,8 @@ async function main() {
         program: kek.fixDit(),
     };
 
-    // it does not even come , because error is happening before.
+    // it never comes here , because error is happening in lines above.
     const stack = await LocalWorkspace.createOrSelectStack(args);
     await stack.workspace.installPlugin('aws', 'v3.38.1');
     await stack.up();
 }
-main()
